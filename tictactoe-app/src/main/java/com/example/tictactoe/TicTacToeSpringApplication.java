cf66be9b1e883f16748771efd82be0e4cb3cde9c
2021-DEV1-066 <batman@gotham.com>
2021-04-27 15:56:18 +0200

    add a gitignore to clean up the repo

---
+++ tictactoe-app/src/main/java/com/example/tictactoe/TicTacToeSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.tictactoe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeSpringApplication.class, args);
+    }
+}
